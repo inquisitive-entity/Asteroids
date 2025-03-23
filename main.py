8d1748aaca53a3538647cc2474f0e5e34055e506
inquisitive-entity <mjjacks88@gmail.com>
2025-03-23 18:49:12 -0500

    movement and groups!

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
     clock = pygame.time.Clock()
     dt = 0
-    #print(f"Screen width / 2: {SCREEN_WIDTH / 2 }")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player( (SCREEN_WIDTH/2), (SCREEN_HEIGHT/2) )
     
     while True:
@@ -19,9 +23,14 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        #player.update(dt)
+        updatable.update(dt)
+        #player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
+        #drawable.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__=="__main__":
     main()
